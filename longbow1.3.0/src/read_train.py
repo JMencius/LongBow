8c492093d75552b160efaa0b89d175ac222651d1
JMencius <63288733+JMencius@users.noreply.github.com>
2024-01-18 15:34:08 +0800

    Add files via upload

---
+++ longbow1.3.0/src/read_train.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def read_qv_train_file(filename : str) -> tuple:
+
+    df = pd.read_csv(filename)
+    df_cleaned = df.dropna()
+    drop_colums = ["Sample", "Config"] + ["R" + str(i) for i in range(0, 91)] + ["id"] + ["Q0"] + ["K" + str(i) for i in range(0, 91)]
+
+    df_final = df_cleaned.drop(columns = drop_colums, axis = 1)
+    
+    y = df_final["Label"].astype(int)
+    X = df_final.drop("Label", axis = 1)
+    X = X.values
+
+    return (X, y)
+
+
+def read_autocorr_train_file(filename : str) -> tuple:
+    df = pd.read_csv(filename)
+    df_cleaned = df.dropna()
+    drop_colums = ["Sample", "Config"]
+
+    df_final = df_cleaned.drop(columns = drop_colums, axis = 1)
+
+    y = df_final["Label"].astype(int)
+    X = df_final.drop("Label", axis = 1)
+    X = X.values
+
+    return (X, y)
+
+
+
+
+"""
+if __name__ == "__main__":
+    test = read_train_file("/home/mencius/test/longbow1.2.0/model/train_guppy.csv")
+    for i in test[0]:
+        print(i)
+"""
