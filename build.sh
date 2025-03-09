5095aae7b90a33a2c98c9699b2cf629d977d78bc
JMencius <553843771@qq.com>
2025-03-09 20:20:58 +0800

    Update files PR to bioconda

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
 mkdir -p $PREFIX/bin;
-cp -r $SRC_DIR/src/longbow/* $PREFIX/bin;
-chmod -R +x $PREFIX/bin;
+cp $SRC_DIR/src/longbow/longbow.py $PREFIX/bin;
+
+mkdir -p $PREFIX/bin/module;
+cp $SRC_DIR/src/longbow/module/* $PREFIX/bin/module;
+
+mkdir -p $PREFIX/bin/model;
+cp $SRC_DIR/src/longbow/model/* $PREFIX/bin/model;
